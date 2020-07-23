59e4f537e102a5fabbf4ac4a595b8a244e3d685c
unknown <guivecchi12@gmail.com>
2020-07-22 19:29:28 -0600

    Start of Home Page

---
+++ Expat-Journal/src/components/HomePage.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { axiosWithAuth } from "../utilities/axiosWithAuth";
+
+
+const HomePage = () => {
+    return(
+        <>
+            <h1>Created</h1>
+        </>
+    )
+}
+
+export default HomePage;
