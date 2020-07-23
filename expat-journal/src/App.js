9ae867207045469ed7b5af9c0b792699ff8c9134
unknown <guivecchi12@gmail.com>
2020-07-22 20:05:20 -0600

    Functional

--- expat-journal/src/App.js
+++ expat-journal/src/App.js
@@ -9,8 +9,8 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <Route exact path="/" component={Login} />
-        <Route exact path="/signup" component = {signup} />
+        {/* <Route exact path="/" component={Login} /> */}
+        {/* <Route exact path="/signup" component = {signup} /> */}
         <PrivateRoute exact path="/protected" component = {HomePage}/>
       </div>
     </Router>
