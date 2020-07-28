d34e61bbf6ae520ce2e580325ce7c2d04c4c2c3a
Alex Vargas <yoralexvargas@gmai.com>
2020-07-28 15:56:00 -0400

    having errors on text field

--- Expat-Journal/src/components/SignUp.js
+++ Expat-Journal/src/components/SignUp.js
@@ -1,11 +1,22 @@
 import React, {useState} from 'react';
+import * as yup from 'yup'
+import styled from 'styled-components';
+
+const schema = yup.object().shape({
+    username: yup.string().required('Please enter your username.').min(3,'Not real name'),
+    email: yup.string().required('Please enter your email.').matches(/^[0-9]{10}$/),
+    password: yup.string().required('Please enter your password.').min(8,),
+    comfirmpassword: yup.string().required('Please recomfirm your password.').min(8,),
+}
+)
+    
 
 
 const defaultFormState = {
-    user_name: '',
+    username: '',
     email: '',
     password: '',
-    comfirm_password: '',
+    comfirmpassword: '',
 
 }
 const defaultErrorState= {
@@ -16,13 +27,25 @@ const SignUp = props => {
     const [formState,setFormState] = useState(defaultFormState);
     const [errors, setErrors] = useState(defaultErrorState);
     const[isDisable, setIsDisable] = useState(true);
+
+    const validate = e =>{
+        yup.reach(schema, e.target.username).validate(e.target.value,)
+        .then(valid => setErrors({...errors, [e.target.username]:''}))
+        .catch(err => setErrors({...errors,[e.target.username]:err.errors[0]}))
+    }
+
+
+
  // redo the handle 
     const handleChange = e => {
         if (e.target.type ==='textkbox'){
             setFormState({
                 ...formState,
-                [e.target.name]: e.target.value
+                [e.target.username]: e.target.value
             })
+        }
+        if (e.target.username === 'username' || e.target.email === 'email') {
+            validate(e);
         }
     }
 
@@ -31,30 +54,38 @@ const SignUp = props => {
         console.log(formState)
     }
     return (
-        <div>
+        <FormContainer>
             <form onSubmit={handleSumbmit}>
-            <h3>Sign up Now!</h3>
-            <label>
-                <fieldset>
-                User Name <input type='text' name='user_name' onChange={handleChange} data-cy='full_name' value={formState.user_name} />
-                </fieldset>
-            </label>
-            <label>
-                <fieldset>
-                Password <input type='text' name='password' onChange={handleChange} data-cy='password' value={formState.password} />
-                </fieldset>
-            </label>
-            <label>
+            <h1>Sign up Now!</h1>
             <fieldset>
-            Comfirm Password <input type='text' name='comfirm_password' onChange={handleChange} data-cy='comfirm_password' value={formState.comfirm_password} />
-               </fieldset>
+            <label>
+                User Name <input type='text' name='username' onChange={handleChange} data-cy='username' value={formState.username} />
+                {errors.username > 0 && <p style={{color:'red'}}>{errors.username}</p> }
             </label>
+            <label>
+                Password <input type='text' name='password' onChange={handleChange} data-cy='password' value={formState.password} />
+            </label>
+            <label>
+            Comfirm Password <input type='text' name='comfirm_password' onChange={handleChange} data-cy='comfirmpassword' value={formState.comfirmpassword} />
+            </label>
+            </fieldset>
             <button type='cancel'> Cancel </button>
-            <button type='submit'> Comfirm </button>
-            </form>
+            <button type='submit'> Submit </button>
+        <div>
+            <p>Have an account?</p>
+            
         </div>
+            </form>
+        </FormContainer>
     );
 }
+
+const FormContainer = styled.div`
+margin: 5rem, auto;
+width: 600px;
+`
+
+
 export default SignUp; 
 
 
