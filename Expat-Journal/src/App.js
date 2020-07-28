bf2712dbe55bbb466ee317332eac83df2f856351
Jorge Jimenez <calenocam24@gmail.com>
2020-07-27 21:14:54 -0400

    fixed app.js

--- Expat-Journal/src/App.js
+++ Expat-Journal/src/App.js
@@ -1,34 +1,33 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Login from "./components/Login";
 import SignUp from "./components/SignUp";
-import Header from './components/Header';
-import Footer from './components/Footer';
-import HomePage from './components/HomePage';
-import PrivateRoute from './utilities/PrivateRoute';
-import './App.css';
-import ImagesList from './components/ImagesList';
-import ImagesInfo from './components/ImagesInfo'
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomePage from "./components/HomePage";
+import PrivateRoute from "./utilities/PrivateRoute";
+import "./App.css";
+import ImagesList from "./components/ImagesList";
+import ImagesInfo from "./components/ImagesInfo";
 
 function App() {
-    return (
-      <Router>
-        <div className="App">
+  return (
+    <Router>
+      <div className="App">
         <Header />
         <Route exact path="/login">
           <Login />
-        </ Route> 
+        </Route>
         <Route exact path="/images">
           <ImagesList />
-        </ Route> 
-        <Route  path="/images/:id">
+        </Route>
+        <Route path="/images/:id">
           <ImagesInfo />
-        </ Route> 
+        </Route>
         <Footer />
-          {/* <Route exact path="/" component={Login} /> */}
-          <Route exact path="/signup" component = {SignUp} />
-          <PrivateRoute exact path="/protected" component={HomePage} />
-        </div>
+        {/* <Route exact path="/" component={Login} /> */}
+        <Route exact path="/signup" component={SignUp} />
+        <PrivateRoute exact path="/protected" component={HomePage} />
       </div>
     </Router>
   );
