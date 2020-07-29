87409ef178c2cda97a56911ae89168c0f262e1ac
unknown <guivecchi12@gmail.com>
2020-07-29 17:17:19 -0600

    functionality done

--- Expat-Journal/src/components/addImage.js
+++ Expat-Journal/src/components/addImage.js
@@ -1,34 +1,82 @@
 import React, { useEffect, useState } from "react";
 import { axiosWithAuth } from "../utilities/axiosWithAuth";
 
+const initialState = {
+    id: 0,
+    img_url: "",
+    user_id: 0
+}
 
 const AddImage = ({ user }) => {
-    
-    const [ imgs, setImgs ] = useState([]);
-    const [ newImg, setNewImg ] = useState({ img:"" });
-    
+
+    // console.log(user);
+
+    const [imgs, setImgs] = useState([]);
+    const [addingImg, setAddingImg] = useState(initialState);
+    const [edit, setEdit] = useState(false);
+    const [editImg, setEditImg] = useState(initialState);
+
     const getImages = () =>{
         axiosWithAuth()
             .get(`/users/${user.id}/images`)
             .then(res => {
-                console.log("addImages GET: ", res);
-                setImgs(res);
+                // console.log("addImages GET: ", res.data);
+                setImgs(res.data);
+                setAddingImg({...addingImg, img_url: "", user_id: user.id});
             })
             .catch(err => console.log("addImages GET error", err))
     }
 
     useEffect(()=>{
         getImages();
+        // setAddingImg({...addingImg, img_url: ""});
     },[]);
+
 
     const addImg = e => {
         e.preventDefault();
-        console.log("the new img: ", newImg);
-        setImgs([...imgs, newImg.img]);
-        console.log("your imgs: ", imgs);
-
+        
+        axiosWithAuth()
+        .post(`/users/${addingImg.user_id}/images/`, addingImg)
+        .then(res=> {
+            console.log( "Response from POST adding image: ", res);
+            getImages();
+        })
+         .catch (err => console.log(err));
     }
 
+    const deleteImg = img => {
+        axiosWithAuth()
+            .delete(`/users/${img.user_id}/images/${img.id}`)
+            .then(res => {
+                console.log(res);
+                getImages();
+            })
+            .catch(err => {console.log(err)})
+    }
+
+    const editingImg = img => {
+        setEdit(true);
+        setEditImg(img);
+    }
+
+    const saveEdit = e => {
+        e.preventDefault();
+        axiosWithAuth()
+            .put(`/users/${user.id}/images/${editImg.id}`, editImg)
+            .then(() => {
+                setEdit(false);
+                getImages();
+            })
+            .catch(err => console.log(err))
+    }
+
+    const checkStates = () =>{
+        console.log("imgs: ",imgs);
+        console.log("addingImg: ", addingImg);
+        console.log("Image editing", editImg);
+    }
+    
 
     return(
         <>
@@ -36,16 +84,39 @@ const AddImage = ({ user }) => {
             <form onSubmit={addImg}>
                 <legend>Add a Image</legend>
                 <label>
-                    Url: 
+                    URL: 
                     <input
                         type="text"  
                         name = "url"
-                        onChange = { e => setNewImg({...newImg, img: e.target.value })}
-                        value = { newImg.img }
-                    />
+                        onChange = {e => {e.persist(); setAddingImg({...addingImg, img_url: e.target.value, id: imgs.length + 1 })}}
+                        value = { addingImg.img_url }
+                    />  
                 </label>
                 <button>add</button>
             </form>
+            {imgs.map(pic=>(
+                <div key = {pic.id} onClick ={() => editingImg(pic)}>
+                    <img src={pic.img_url}/>
+                    <span className = "delete" onClick = {e => {
+                         e.stopPropagation(); 
+                         deleteImg(pic) }}
+                    > x </span>
+                </div>
+            ))}
+            {edit && (
+                <form onSubmit = {saveEdit}>
+                    <legend>edit image URL</legend>
+                    <label>
+                        URL: 
+                        <input
+                            onChange = {e => {e.persist(); setEditImg({...editImg, img_url: e.target.value}); console.log(editImg) }}
+                            value = { editImg.img_url }
+                        />   
+                    </label>
+                    <button type="submit">save</button>
+                </form>
+            )}
+            <button onClick = {checkStates}>Check States</button>
 
         </>
     )
