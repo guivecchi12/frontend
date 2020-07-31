2ff9d299e0e1f6b0d9524d9a223730e720e70f40
William Sedlacek <wsedlacekc@gmail.com>
2020-07-30 19:35:06 -0700

    Revert "Revert "Context""

--- Expat-Journal/src/index.js
+++ Expat-Journal/src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { BrowserRouter as Router, withRouter } from 'react-router-dom';
+
+const AppWithRouter = withRouter(App);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+		<AppWithRouter />
+	</Router>,
   document.getElementById('root')
 );
 
