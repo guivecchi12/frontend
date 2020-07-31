cd96d1795b734627bf1ca0af2c9d2c74a0f19731
Jose Paquian <alostnight1993@outlook.com>
2020-07-30 21:46:25 -0500

    Removed Image Info

--- Expat-Journal/src/components/HomePage.js
+++ Expat-Journal/src/components/HomePage.js
@@ -3,14 +3,23 @@ import { Link } from "react-router-dom";
 import Header from "./components/Header";
 import ImgGit from "./ImgGit";
 import StoriesList from "./StoriesList";
+import styled from "styled-components";
+
+const ImgStor = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
 
 const HomePage = () => {
   return (
-    <>
+    <div className="container">
+      <Header />
       <h1>Our Journal</h1>
-      <StoriesList />
-      <ImgGit />
-    </>
+      <ImgStor>
+        <StoriesList />
+        <ImgGit />
+      </ImgStor>
+    </div>
   );
 };
 
