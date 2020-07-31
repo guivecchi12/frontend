dfce28b07e69dbf23c5cab2610a02a772c4f528e
Jorge Jimenez <calenocam24@gmail.com>
2020-07-30 20:00:53 -0400

    added styles

--- Expat-Journal/src/components/HomePage.js
+++ Expat-Journal/src/components/HomePage.js
@@ -1,20 +1,22 @@
 import React from "react";
 import { axiosWithAuth } from "../utilities/axiosWithAuth";
-import { Link } from 'react-router-dom';
-import StoriesList from './StoriesList'
+import { Link } from "react-router-dom";
+import StoriesList from "./StoriesList";
+import styled from "styled-components";
 
+const Stories = styled.div`
+margin-left: 30px;
+`
 
 const HomePage = () => {
-    return(
-        <>
-            <h1>Created</h1>
-            <Link to="/addImages">Click to see your images</Link>
-            <div>
-                <StoriesList />
-            </div>
-
-        </>
-    )
-}
+  return (
+    <>
+      <h1>Created</h1>
+      <Stories>
+        <StoriesList />
+      </Stories>
+    </>
+  );
+};
 
 export default HomePage;
--- Expat-Journal/src/components/StoriesList.js
+++ Expat-Journal/src/components/StoriesList.js
@@ -1,20 +1,51 @@
 import React, { useState, useEffect } from "react";
 import axiosWithAuth from "../utilities/axiosWithAuth";
 import StoriesGetCard from "./StoriesGetCard";
+import styled from "styled-components";
+import { Link } from 'react-router-dom';
+
+const StoriesContainer = styled.div`
+  margin: 20px;
+  width: 500px;
+  margin: 8px 0 0 1px;
+      border: 2px solid black;
+      border-radius: 6px;
+      padding: 10px 20px;
+      font-size: 1.3rem;
+  .button {
+    text-align: center;
+    padding-bottom: 30px;
+    button {
+      width: 150px;
+      background-color: black;
+      color: white;
+      font-size: 1.2rem;
+      margin: 30px 0 0 0;
+      padding: 5px 9px;
+      cursor: pointer;
+      border: 2px black solid;
+      border-radius: 5px;
+      &:hover {
+        background-color: #778899;
+        color: #f0fff0;
+      }
+    }
+  }
+`;
 
 // const dummyData = [
-    
-//     {   
+
+//     {
 //         id: 1,
 //         story_title: "A Place To Be",
 //         story_body: "Lorem ipsum, lorem ipsum dolor sit amet, Lorem, dummy text, loren ipsum (yes, spelled wrong), Lorem ipsum sample textipsum loremlorem ipsum sample, Latin copy text, Lorem ipsum text, Latin dummy text, template text, sample text, dummy copy text, Latin sample text, HTML dummy text, Lorem ipsum dummy text."
 //     },
-//     {   
+//     {
 //         id: 2,
 //         story_title: "It Was Too Scary That Night",
 //         story_body: "Lorem ipsum, lorem ipsum dolor sit amet, Lorem, dummy text, loren ipsum (yes, spelled wrong), Lorem ipsum sample textipsum loremlorem ipsum sample, Latin copy text, Lorem ipsum text, Latin dummy text, template text, sample text, dummy copy text, Latin sample text, HTML dummy text, Lorem ipsum dummy text"
 //     },
-//     {   
+//     {
 //         id: 3,
 //         story_title: "I Can't Believe That I Spent that Much",
 //         story_body: "Lorem ipsum, lorem ipsum dolor sit amet, Lorem, dummy text, loren ipsum (yes, spelled wrong), Lorem ipsum sample textipsum loremlorem ipsum sample, Latin copy text, Lorem ipsum text, Latin dummy text, template text, sample text, dummy copy text, Latin sample text, HTML dummy text, Lorem ipsum dummy text."
@@ -24,7 +55,6 @@ import StoriesGetCard from "./StoriesGetCard";
 
 const StoriesList = () => {
   const [storiesList, setStoriesList] = useState([]);
-
 
   useEffect(() => {
     axiosWithAuth()
@@ -39,23 +69,23 @@ const StoriesList = () => {
   }, []);
 
   return (
-
-    <>
-    <div>
-      <button>Add Story</button>
-    </div>
-      <div>
-        <ol>
-          {storiesList.map((story) => (
-            <StoriesGetCard key={story.id} 
+    <StoriesContainer>
+      <Link to="/addStory">
+      <div className="button">
+        <button>Add Story</button>
+      </div>
+      </Link>
+      <ol>
+        {storiesList.map((story) => (
+          <StoriesGetCard
+            key={story.id}
             story_title={story.story_title}
             story_body={story.story_body}
-            story={story} />
-          ))}
-        </ol>
-      </div>
-    </>
-
+            story={story}
+          />
+        ))}
+      </ol>
+    </StoriesContainer>
   );
 };
 
